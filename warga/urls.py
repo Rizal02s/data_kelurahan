b930be646f94b8057f1ccf17e9cf0a9efe44d482
Rizal Andi <rizalsudjay05@gmail.com>
2025-11-12 16:09:20 +0700

    Update Terbaru

--- warga/urls.py
+++ warga/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import WargaListView, WargaDetailView, PengaduanListView
 
 urlpatterns = [
-    path('', WargaListView.as_view(), name='warga-list'),
-    path('', PengaduanListView.as_view(), name='warga-list'),
-    path('<int:pk>/', WargaDetailView.as_view(), name=''),
+    path('', WargaListView.as_view(), name='warga-list'),  # /warga/
+    path('pengaduan/', PengaduanListView.as_view(), name='pengaduan-list'),  # /warga/pengaduan/
+    path('<int:pk>/', WargaDetailView.as_view(), name='warga-detail'),  # /warga/1/
 ]
